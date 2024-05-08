427a7582d41ee58afc95a765d45c04ef42e81743
Arturo Palau <arturo.palau@sonarsource.com>
2024-05-08 11:29:23 -0500

    project search, latest analysis and oldest analysis

--- src/get_sq_data.py
+++ src/get_sq_data.py
@@ -1,6 +1,10 @@
 import requests
 import json
+import sys
 
+# Get the command-line argument
+arg1 = sys.argv[1] if len(sys.argv) > 1 else None
+arg2 = sys.argv[2] if len(sys.argv) > 2 else None
 
 def query_api(url, headers=None):
     try:
@@ -8,7 +12,7 @@ def query_api(url, headers=None):
         response.raise_for_status()
         data = response.json()
         # Process the data here
-        print(data)
+        #print(data)
         return data
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
@@ -17,14 +21,19 @@ def save_to_json(data, filename):
     with open(filename, 'w') as file:
         json.dump(data, file)
 
-# Example usage
-new_api_url = "https://artpal.ngrok.io/api/projects/license_usage"
-response_data = query_api(new_api_url, headers={"Authorization": "Bearer squ_cefbc007eaa5a641017356b852f32598d0f6ec76"})
-filename = new_api_url.split("/")[-2] + "-" + new_api_url.split("/")[-1] + ".json"
-save_to_json(response_data, filename)
+api_calls = {
+    "license_usage": "/api/projects/license_usage",
+    "quality_gates": "/api/qualitygates/list",
+    "projects_latest_analysis": "/api/components/search_projects?f=analysisDate&s=analysisDate&asc=false",
+    "projects_oldest_analysis": "/api/components/search_projects?f=analysisDate&s=analysisDate&asc=true"
+}
 
-# Additional usage
-new_api_url = "https://artpal.ngrok.io/api/qualitygates/list"
-response_data = query_api(new_api_url, headers={"Authorization": "Bearer squ_cefbc007eaa5a641017356b852f32598d0f6ec76"})
-filename = new_api_url.split("/")[-2] + "-" + new_api_url.split("/")[-1] + ".json"
-save_to_json(response_data, filename)
+# Use the argument in your code
+if arg1 and arg2:
+    if arg1.endswith('/'):
+        arg1 = arg1[:-1]
+    for key, value in api_calls.items():
+        new_api_url = arg1 + value
+        response_data = query_api(new_api_url, headers={"Authorization": "Bearer " + arg2})
+        filename = key + ".json"
+        save_to_json(response_data, filename)
